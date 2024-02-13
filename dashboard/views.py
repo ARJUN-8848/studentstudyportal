43c24f2e3b2eecd0b44118200a6fc3087da8c156
Arjun S <153733277+ARJUN-8848@users.noreply.github.com>
2024-02-13 17:47:22 +0530

    Add files via upload

---
+++ dashboard/views.py
@@ -0,0 +1,194 @@
+
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from .models import Homework, Notes, Todo
+from .forms import HomeworkForm, NotesForms, DashboardFom, TodoForm, UserRegistrationForm
+from django.views import generic
+from youtubesearchpython import VideosSearch
+
+def home(request):
+    return render(request, 'dashboard/home.html')
+
+def notes(request):
+    # if request.method == "POST": 
+    if request.method == "POST":
+
+        form = NotesForms(request.POST)
+        if form.is_valid():
+            notes = Notes(user=request.user, title=request.POST['title'], description=request.POST['description'])
+            notes.save()
+            messages.success(request, f"Notes added from {request.user.username} successfully!")
+    else:
+        form = NotesForms()
+
+    notes = Notes.objects.filter(user=request.user)
+    context = {'notes': notes, 'form': form}
+    return render(request, 'dashboard/notes.html', context)
+
+def delete_note(request, pk=None):
+    Notes.objects.get(id=pk).delete()
+    return redirect("notes")
+
+class NotesDetailView(generic.DetailView):
+    model = Notes
+
+def homework(request):
+    if request.method == "POST":
+        form = HomeworkForm(request.POST)
+        if form.is_valid():
+            try:
+                finished = request.POST.get('is_finished', False)
+            except:
+                finished = False
+
+            homework = Homework(
+                user=request.user,
+                subject=request.POST['subject'],
+                title=request.POST['title'],
+                description=request.POST['description'],
+                due=request.POST['due'],
+                is_finished=finished
+            )
+            homework.save()
+            messages.success(request, f"Homework added from {request.user.username} successfully!")
+        else:
+            form = HomeworkForm()
+    else:
+        form = HomeworkForm()
+
+    homework = Homework.objects.filter(user=request.user)
+    if len(homework) == 0:
+        homework_done = True
+    else:
+        homework_done = False
+
+    context = {'homeworks': homework, 'homeworks_done': homework_done, 'form': form}
+    return render(request, 'dashboard/homework.html', context)
+
+def update_homework(request, pk=None):
+    homework = Homework.objects.get(id=pk)
+    if homework.is_finished == True:
+        homework.is_finished = False
+    else:
+        homework.is_finished = True
+    homework.save()
+    return redirect('homework')
+
+def delete_homework(request, pk=None):
+    Homework.objects.get(id=pk).delete()
+    return redirect('homework')
+
+def youtube(request):
+    if request.method == "POST":
+        form = DashboardFom(request. POST)
+        text =request.POST['text']
+        video=VideosSearch(text,limit=10)
+        result_list=[]
+        for i in video.result()['result']:
+            result_dict={
+                'input':text,
+                'title':i['title'],
+                'duration':i['duration'],
+                'thumbnails':i['thumbnails'][0]['url'],
+                'channel':i['channel']['name'],
+                'link':i['link'],
+                'views':i['viewCount']['short'],
+                'published':i['publishedTime']
+                
+            }
+            desc = ''
+            if i['descriptionSnippet']:
+                for j in i[ 'descriptionSnippet']:
+                    desc += j['text']
+            result_dict[ 'description'] = desc
+            result_list.append(result_dict)
+            context={
+                'form':form,
+                'results':result_list
+            }
+            
+        return render(request,'dashboard/youtube.html',context)
+    else:
+        form = DashboardFom()
+    context = {'form': form}
+    return render(request, "dashboard/youtube.html", context)
+def todo(request):
+    if request.method =='POST':
+        form=TodoForm(request.POST)
+        if form.is_valid():
+            try:
+                finished=request.POST["is_finished"]
+                if finished=='on':
+                    finished=True
+                else:
+                    finished=False
+            except:
+                finished=False
+            todos=Todo(
+                user=request.user,
+                title=request.POST['title'],
+                is_finished=finished
+                
+            )
+            todos.save()
+            messages.success(request, f"Todo added from {request.user.username} successfully!")
+    else:
+        form= TodoForm()
+    todo=Todo.objects.filter(user=request.user)
+    if len (todo)==0:
+        todos_done=True
+    else:
+        todos_done=False
+
+        
+    
+    context={
+        'form':form,
+        'todos':todo,
+        'todos_done':todos_done
+    }
+    return render (request, "dashboard/todo.html",context)
+def update_todo (request, pk=None):
+    todo = Todo.objects.get(id=pk)
+    if todo.is_finished == True:
+        todo.is_finished = False
+    else:
+        todo. is_finished = True
+    todo. save()
+    return redirect ('todo')
+def delete_todo (request, pk=None):
+    Todo.objects.get(id=pk).delete()
+    return redirect("todo")
+
+def register(request):
+    if request.method =='POST':
+        form=UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            messages.success(request,f"ACCOUNT CREATED FOR {username}!!")
+            return redirect("login")
+    else:
+        form=UserRegistrationForm()
+    context={
+            'form':form
+    }
+    return render(request,"dashboard/register.html",context)
+def profile(request) :
+    homeworks=Homework.objects.filter(is_finished=False,user=request.user)
+    todos=Todo.objects.filter(is_finished=False,user=request.user)
+    if len(homeworks)==0:
+        homework_done=True
+    else:
+        homework_done=False
+    if len(todos)==0:
+        todos_done=True
+    else:
+        todos_done=False
+    context={
+        'homeworks':homeworks,
+        'todos':todos,
+        'homework_done':homework_done,
+        'todos_done':todos_done
+    }
+    return render(request,"dashboard/profile.html",context)
